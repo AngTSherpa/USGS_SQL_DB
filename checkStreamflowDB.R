a469595c2b7e41cdf4614fe463e647878fa7a9b3
James McCreight <jamesmcc@ucar.edu>
2016-07-12 11:09:46 -0600

    minor touchups

--- checkStreamflowDB.R
+++ checkStreamflowDB.R
@@ -3,15 +3,27 @@
 df1 <- data.frame()
 df2 <- data.frame()
 
-checkStreamflowDB <- function (df1, df2){
-  if (identical(df1, df2, single.NA = FALSE)){
+checkStreamflowDB <- function (df1, df2, tolerance=1e-6){
+
+  if (identical(df1, df2)){
     print ("Datas are same")
   }
   
   else {
-    for (c in 1:ncol(df1)){
-      result <- identical(df1[,c],df2[,c], single.NA = FALSE)
-      print (paste(c,result))
+
+      names1 <- names(df1)
+      names2 <- names(df2)
+      sameNames <- intersect(names1,names2)
+      
+    for (nn in sameNames){
+      cat(nn,'\n')
+      ##result <- identical(df1[,c],df2[,c], single.NA = FALSE)
+      result <- all(df1[,nn]==df2[,nn], na.rm=TRUE)
+
+      if(!result) result <- all(abs(df1[,nn]-df2[,nn]) < tolerance, na.rm=TRUE)
+      
+      cat(result,'\n')
+      cat('\n')
     }
   } 
 }
